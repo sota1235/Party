75880017f081b50e23712ca27003eacbcb934c41
sota1235 <sota1235@gmail.com>
2015-08-16 18:53:55 +0900

    modify: install assets and run coffee, sass / add config.ru

--- app.rb
+++ app.rb
@@ -1,7 +1,33 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/assetpack'
+require 'sinatra/reloader'
 require 'slim'
+require 'sass'
+require 'coffee-script'
 
+class AllStarThanksGiving < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    register Sinatra::AssetPack
+  end
 
-get '/' do
-  slim :index
+  get '/' do
+    slim :index
+  end
+
+  # js and css
+  assets do
+    serve '/js', :from => 'assets/js'
+    js :application, [
+      'js/*.js'
+    ]
+
+    serve '/css', :from => 'assets/css'
+    css :application, [
+      '/css/*.css'
+    ]
+
+    js_compression :jsmin
+    css_compression :sass
+  end
 end
