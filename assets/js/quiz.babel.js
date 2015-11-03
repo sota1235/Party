7f0af80bc8e3c967544dc07b8d9165a82ef72494
sota1235 <sota1235@gmail.com>
2015-11-03 10:34:56 +0100

    modify: transplant quiz views to react

--- assets/js/quiz.babel.js
+++ assets/js/quiz.babel.js
@@ -9,10 +9,69 @@
  */
 'use strict';
 
-var $       = require('jquery');
-var Comment = require('./comments.babel.js');
+var React    = require('react');
+var ReactDOM = require('react-dom');
+var $        = require('jquery');
+var Comment  = require('./comments.babel.js');
 
 var socket  = io();
+
+/* React components */
+var Answers = React.createClass({
+  getInitialState: () => {
+    return {
+      data: [
+        {num: 'A1', vote: 0, key: 1},
+        {num: 'A2', vote: 0, key: 2},
+        {num: 'A3', vote: 0, key: 3},
+        {num: 'A4', vote: 0, key: 4},
+      ]
+    };
+  },
+  render: function() {
+    return (
+      <div className="answers">
+        <AnswerDisplayList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+var AnswerDisplay = React.createClass({
+  render: function() {
+    return (
+      <div className="answerDisplay" key={this.props.key}>
+        <h1>{this.props.num}</h1>
+        <div>回答者数
+          <span>{this.props.children}</span>
+        </div>
+      </div>
+    );
+  }
+});
+
+var AnswerDisplayList = React.createClass({
+  render: function() {
+    var displayNodes = this.props.data.map(function(displays) {
+      return (
+        <AnswerDisplay num={displays.num} key={displays.key}>
+          {displays.vote}
+        </AnswerDisplay>
+      );
+    });
+    return (
+      <div className="answerDisplayList">
+        {displayNodes}
+      </div>
+    )
+  }
+});
+
+/* React rendering */
+ReactDOM.render(
+  <Answers />,
+  document.getElementById('answers')
+);
 
 // 指定されたjquery Objectの数字をカウントアップする
 var countUp = (selector) => {
