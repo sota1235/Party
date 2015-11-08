b8577d817eca948bae4269e3ed802cff0c154dd3
sota1235 <sota1235@gmail.com>
2015-11-08 15:29:04 +0100

    modify: use react-bootstrap(sample)

--- assets/js/admin.babel.js
+++ assets/js/admin.babel.js
@@ -12,15 +12,50 @@
 import React      from 'react';
 import { render } from 'react-dom';
 import $          from 'jquery';
+// react bootstrap
+import {
+  ListGroup, ListGroupItem, Panel
+} from 'react-bootstrap';
 
-var socket = io();
+var socket    = io();
+var Component = React.Component;
 
-/* make quiz form */
-class QuizAdmin extends React.Component {
+/* commponents */
+
+// question
+class Question extends Component {
   render() {
     return (
-      <div className='quizAdmin'>
-        <h1>Hello, quiz admin</h1>
+      <div className='question'>
+        <Panel collapsible defaultExpanded header="Panel heading">
+          <ListGroup fill>
+            <ListGroupItem>Item 1</ListGroupItem>
+            <ListGroupItem>Item 2</ListGroupItem>
+          </ListGroup>
+        </Panel>
+      </div>
+    );
+  }
+}
+
+// question list
+class QuestionList extends Component {
+  render() {
+    return (
+      <div className='questionList'>
+        <Question />
+      </div>
+    );
+  }
+}
+
+// admin page
+class QuestionAdmin extends Component {
+  render() {
+    return (
+      <div className='questionAdmin'>
+        <h1>Hello, question admin</h1>
+        <QuestionList />
       </div>
     );
   }
@@ -28,6 +63,6 @@ class QuizAdmin extends React.Component {
 
 /* rendering */
 render(
-  <QuizAdmin />,
-  document.getElementById('quiz-admin')
+  <QuestionAdmin />,
+  document.getElementById('question-admin')
 );
