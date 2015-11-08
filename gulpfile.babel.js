f0a248d3c5704d970a6b0a8fb45b2ca4b96fd7b4
sota1235 <sota1235@gmail.com>
2015-11-08 14:44:59 +0100

    install: add watchify and make gulp task to watch js files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,40 +6,80 @@ import sass       from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import minifyCss  from 'gulp-minify-css';
 import postcss    from 'gulp-postcss';
+import duration   from 'gulp-duration';
 // others
 import browserify   from 'browserify';
+import watchify     from 'watchify';
 import babelify     from 'babelify';
 import autoprefixer from 'autoprefixer';
 import source       from 'vinyl-source-stream';
 import buffer       from 'vinyl-buffer';
 import glob         from 'glob';
 
+/* functions */
 // ./hote/moge/common.babel.js => common.js
 var makeDestName = (fileName) => {
   var babel = fileName.split('/').reverse()[0];
   return babel.replace(/\.babel/, '');
 };
 
+// build js files with options
+var buildScripts = (script, isWatch) => {
+  var defaultOption = {
+    entries: [script],
+    //extensions: ['.jsx'],
+    debug: true
+  };
+  // choose bundler
+  var bundler = null;
+  if(isWatch) {
+    let option = defaultOption;
+    option.cache        = {};
+    option.packageCache = {};
+
+    bundler = watchify(browserify(option));
+  } else {
+    bundler = browserify(defaultOption);
+  }
+  // function for build script
+  var rebundle = () => {
+    bundler
+      .transform(babelify, {
+        presets: ['es2015', 'react'],
+        plugins: ['transform-react-display-name']
+      })
+      .bundle()
+      .on('error', (err) => {
+        console.log(`Error: #{err.message}`)
+      })
+      .pipe(source(makeDestName(script)))
+      .pipe(duration(`Compiled '#{script}'`))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({
+        loadMaps: true
+      }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./public/js'));
+  };
+
+  // listen file's update
+  bundler.on('update', rebundle)
+
+  return rebundle();
+};
+
+/* tasks */
 gulp.task('script', () => {
   let scripts = glob.sync('./assets/js/*.js');
   for(let script of scripts) {
-    browserify({
-      entries: [script],
-      debug: true
-    })
-    .transform(babelify, {
-      presets: ['es2015', 'react'],
-      plugins: ['transform-react-display-name']
-    })
-    .bundle()
-    .on('error', (err) => { console.log('Error: ' + err.message); })
-    .pipe(source(makeDestName(script)))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({
-      loadMaps: true
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./public/js'));
+    buildScripts(script, false);
+  }
+});
+
+gulp.task('watchify', () => {
+  let scripts = glob.sync('./assets/js/*.js');
+  for(let script of scripts) {
+    buildScripts(script, true);
   }
 });
 
