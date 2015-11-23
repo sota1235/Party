f968e8b0c201d574a0e13fc10906b1318bb7e744
sota1235 <sota1235@gmail.com>
2015-11-23 08:50:35 +0100

    add: React component of quiz timer

---
+++ assets/js/components/TimerComponent.jsx
@@ -0,0 +1,48 @@
+/**
+ * TimterComponent.jsx
+ *
+ * Description:
+ *   React Component for measuring the time
+ *
+ * Author:
+ *   @sota1235
+ */
+'use strict';
+
+import React from 'react';
+
+var Component = React.Component;
+
+/* React components */
+export default class TimerComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 15,
+      style: {
+        textAlign: 'center'
+      }
+    }
+    this.countDown = this.countDown.bind(this);
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(this.countDown, 1000);
+  }
+
+  countDown() {
+    let time = this.state.time;
+    this.setState({time: time - 1});
+    if(this.state.time === 0) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  render() {
+    return (
+      <div className="timer">
+        <h1 style={this.state.style}>{this.state.time}</h1>
+      </div>
+    );
+  }
+}
