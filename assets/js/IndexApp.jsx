a0dd41cb96957e4a69159f96b09e5cd7763e508b
sota1235 <sota1235@gmail.com>
2015-12-05 16:05:55 +0100

    modify: add validation for comment #25

--- assets/js/IndexApp.jsx
+++ assets/js/IndexApp.jsx
@@ -13,6 +13,26 @@ import $ from 'jquery';
 var socket = io();
 
 $(() => {
+  /* Functions */
+  // validate comment
+  var validateComment = comment => {
+    if(comment.length > 20) {
+      return false;
+    }
+    return true;
+  };
+  // assert validation error of comment
+  var commentValidateError = () => {
+    var $selector = $('.comment-form .err-space');
+    $selector.text('コメントの文字数は20文字以内でお願いします...(´・ω・｀)');
+    setTimeout(() => {
+      $selector.fadeOut('slow', () => {
+        $selector.text('');
+        $selector.fadeIn('slow');
+      });
+    }, 3000);
+  };
+
   /* Event listners */
   // send vote
   $('.choice-form button').on('click', function(event) {
@@ -25,6 +45,9 @@ $(() => {
   $('.comment-form button').on('click', (event) => {
     event.preventDefault();
     var text = $('.comment-form textarea')[0].value;
+    if(!validateComment(text)) {
+      commentValidateError();
+    }
     console.log('send comment');
     socket.emit('comment', text);
     $('.comment-form textarea')[0].value = '';
