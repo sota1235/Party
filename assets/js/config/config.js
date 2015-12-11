b85fe1849850aed24febe20166f76cf19e408d9a
sota1235 <sota1235@gmail.com>
2015-12-11 23:45:01 +0900

    modify: add config files for Party

---
+++ assets/js/config/config.js
@@ -0,0 +1,15 @@
+/**
+ * config.js
+ *
+ * Description:
+ *   Config variable for Party
+ *
+ * Author:
+ *   @sota1235
+ */
+
+import appConfig   from './app.json';
+import styleConfig  from './style.json';
+
+export var app   = appConfig;
+export var style = styleConfig;
