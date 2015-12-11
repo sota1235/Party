6e70decf39ab98212e1f8287c09c4274c1e99805
sota1235 <sota1235@gmail.com>
2015-12-12 00:01:02 +0900

    modify: show title log from config files

--- assets/js/components/Quiz/QuizTitleComponent.jsx
+++ assets/js/components/Quiz/QuizTitleComponent.jsx
@@ -11,6 +11,7 @@
 
 import React from 'react';
 import { EventEmitter2 } from 'eventemitter2';
+import { app, style } from '../../config/config.js';
 
 var Component = React.Component;
 
@@ -18,16 +19,25 @@ var Component = React.Component;
 export default class QuizTitleComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      style: {
-        textAlign: 'center'
-      }
+    this.hStyle = {
+      textAlign: 'center'
+    }
+    this.imgStyle = {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      right: 0,
+      left: 0,
+      margin: 'auto'
     }
   }
   render() {
+    let content = app.imgLogo ?
+      <img style={this.imgStyle} src={style.background} /> :
+      <h1 style={this.hStyle}>{app.title}</h1>;
     return (
       <div className="quizTitleComponent">
-        <h1 style={this.state.style}>Quiz Title</h1>
+        {content}
       </div>
     );
   }
