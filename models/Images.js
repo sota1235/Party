996478e1088f7bb210487a416f74a8c8d38ab6af
sota1235 <sota1235@gmail.com>
2015-12-14 11:37:28 +0100

    modify: add image model and routing

---
+++ models/Images.js
@@ -0,0 +1,106 @@
+/**
+ * Images.js
+ *
+ * Description:
+ *   data model for images
+ *
+ * Author:
+ *   @sota1235
+ */
+
+var mongoose = require('mongoose');
+
+var Schema   = mongoose.Schema;
+
+var Images = function(app) {
+  var ImagesSchema = new Schema({
+    imgName: String
+  });
+
+  mongoose.model('Images', ImagesSchema);
+  mongoose.connect('mongodb://localhost/images');
+
+  var Images;
+
+  /* db initialize */
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log("Connected to 'images' database");
+    Images = mongoose.model('Images');
+  });
+
+  /* Functions */
+  // return all image data
+  var findAll = function() {
+    return new Promise(function(resolve, reject) {
+      console.log('Getting image list');
+
+      Images.find({}, function(err, results) {
+        if (err) {
+          reject({'error': 'An error has occurred'});
+        }
+        console.log('Success: Getting images');
+        resolve(results);
+      });
+    });
+  };
+
+  // get image by _id
+  var findImage = function(req) {
+    return new Promise(function(resolve, reject) {
+      var id = req.params.id;
+      console.log('Getting image that id is ' + id);
+      Images.find({ _id: id }, function(err, result) {
+        if (err) {
+          reject({'error': 'An error has occurred'});
+        }
+        console.log('Success: Getting image');
+        resolve(result);
+      });
+    });
+  };
+
+  // add image data from request body
+  var addImage = function(req) {
+    return new Promise(function(resolve, reject) {
+      var file = req.file.filename;
+      console.log('Adding image: ' + file);
+
+      var addImage = new Images({ imgName: file });
+      addImage.save(function(err, result) {
+        if (err) {
+          reject({'error': err});
+        }
+        console.log('Success: ' + JSON.stringify(result));
+        resolve(result);
+      });
+    });
+  };
+
+  // delete image data by id
+  var deleteImage = function(req) {
+    return new Promise(function(resolve, reject) {
+      var id = req.body.id;
+      console.log('Delete image id: ' + id);
+
+      Images.remove({_id: id}, function(err, result) {
+        if(err) {
+          console.log('Delete image error: ' + JSON.stringify(err));
+          reject(err);
+        }
+        console.log('Success: ' + JSON.stringify(result));
+        resolve(result);
+      });
+    });
+  };
+
+  return {
+    findAll     : findAll,
+    findImage   : findImage,
+    addImage    : addImage,
+    deleteImage : deleteImage
+  };
+};
+
+module.exports.Images = Images();
--- routes/httpRoutes.js
+++ routes/httpRoutes.js
@@ -15,6 +15,7 @@ var upload = multer({ dest: path.join(__dirname, '..', 'public', 'uploads') });
 
 module.exports = function(app) {
   var Questions = app.get('models').Questions;
+  var Images    = app.get('models').Images;
 
   app.get('/', function(req, res, next) {
     res.render('index');
@@ -29,6 +30,7 @@ module.exports = function(app) {
   });
 
   /* access to models */
+  // Questions
   app.post('/upload', upload.single('questionImg'), function(req, res, next) {
     Questions.updateQuestion(req)
       .then(function(result) {;
@@ -72,6 +74,47 @@ module.exports = function(app) {
 
   app.post('/delete/question', function(req, res, next) {
     Questions.deleteQuestion(req)
+      .then(function(result) {
+        res.json(result);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  // Images
+  app.post('/upload/img', upload.single('normalImg'), function(req, res, next) {
+    Images.addImage(req)
+      .then(function(result) {;
+        res.json(result);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.get('/get/images', function(req, res, next) {
+    Images.findAll()
+      .then(function(result) {
+        res.json(result);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.get('/get/image/:id', function(req, res, next) {
+    Images.findImage(req)
+      .then(function(result) {
+        res.json(result);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.post('/delete/image', function(req, res, next) {
+    Images.deleteQuestion(req)
       .then(function(result) {
         res.json(result);
       })
