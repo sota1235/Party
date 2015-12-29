17a1d763041ad1ce5714a0890f957fea22a0a271
sota1235 <sota1235@gmail.com>
2015-12-29 15:59:16 +0100

    modify: add migration file for db #34

--- migrations/20151229142919-create-question-choices.js
+++ migrations/20151229142919-create-question-choices.js
@@ -1,29 +1,34 @@
 /**
- * Create table for questions
+ * Create table for choices of question
  */
 var dbm = global.dbm || require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-  db.createTable('questions', {
-    question_id : {
+  db.createTable('question_choices', {
+    question_choice_id : {
       type          : 'int',
       primaryKey    : true,
       notNull       : true,
       autoIncrement : true,
       unique        : true
     },
+    question_id : {
+      type    : 'int',
+      notNull : true,
+    },
+    choice_index : {
+      type       : 'int',
+      primaryKey : true,
+      notNull    : true
+    },
     text : {
       type    : 'string',
-      notNull : true
-    },
-    answer_index : {
-      type    : 'int',
       notNull : true
     }
   }, callback);
 };
 
 exports.down = function(db, callback) {
-  db.dropTable('questions', callback);
+  db.dropTable('question_choices', callback);
 };
--- migrations/20151229145354-create-question-images.js
+++ migrations/20151229145354-create-question-images.js
@@ -1,29 +1,29 @@
 /**
- * Create table for questions
+ * Create table for images of question's choice
  */
 var dbm = global.dbm || require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-  db.createTable('questions', {
-    question_id : {
+  db.createTable('question_images', {
+    question_image_id : {
       type          : 'int',
       primaryKey    : true,
       notNull       : true,
       autoIncrement : true,
       unique        : true
     },
-    text : {
-      type    : 'string',
-      notNull : true
-    },
-    answer_index : {
+    question_choice_id : {
       type    : 'int',
+      notNull : true,
+    },
+    file_name : {
+      type    : 'string',
       notNull : true
     }
   }, callback);
 };
 
 exports.down = function(db, callback) {
-  db.dropTable('questions', callback);
+  callback();
 };
